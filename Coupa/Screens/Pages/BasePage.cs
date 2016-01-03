67d5a862216e848351f20b5e66e67e7beba659cb
Christopher Clark <g.kanizaj.clark@gmail.com>
2016-01-04 08:00:16 +1100

    latest code 2016

--- Coupa/Screens/Pages/BasePage.cs
+++ Coupa/Screens/Pages/BasePage.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Screens.Pages;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace Screens.Pages
 {
@@ -15,6 +17,35 @@ namespace Screens.Pages
         List<IWebElement> Header { get; set; }
         List<IWebElement> Footer { get; set; }
         #endregion
-    
+
+        #region Selectors
+      //  [FindsBy(How = How.Id, Using = "requisition_header_filter")]
+        [FindsBy(How = How.Name, Using = "filter")]
+        IWebElement filters;
+        #endregion
+
+        #region Code
+        public void filterBy(String filter)
+        {
+            SelectElement se = new SelectElement(filters);
+            se.SelectByText(filter);
+        }
+
+        public void testDropdown()
+        {
+            // Anchor to header of View part of Screen
+            // Assign anchor to a Select WebElement then grab the Options into a List
+            SelectElement se = new SelectElement(filters);
+            List<IWebElement> selects = se.Options.ToList();
+
+            // Iterate through the available Options
+            foreach (var dropdowns in selects)
+            {
+                dropdowns.Click();
+                Thread.Sleep(1000);
+            }
+        }
+        #endregion
+
     }
 }
