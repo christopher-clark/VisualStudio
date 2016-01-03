b475e093e2fb8ae8ed6146b5659811e07c9fb90c
Christopher Clark <g.kanizaj.clark@gmail.com>
2016-01-04 08:00:38 +1100

    latest code 2016

--- Coupa/Screens/Pages/CoupaHomePageObj.cs
+++ Coupa/Screens/Pages/CoupaHomePageObj.cs
@@ -8,16 +8,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Screens.Pages;
+using OpenQA.Selenium.Support.UI;
 
 namespace Screens.Pages
 {
     class CoupaHomePageObj : BasePage
     {
+        public IWebDriver Driver { get; private set; }
+        public WebDriverWait Wait { get; set; }
 
-         #region Findsby
-        [FindsBy(How = How.ClassName, Using = "primary")]
-        IList<IWebElement> pageMenu;
-
+        #region Findsby
         [FindsBy(How = How.Id, Using = "//*[@id='topNav']/text()")]
         IWebElement user;
         
@@ -30,7 +30,30 @@ namespace Screens.Pages
         [FindsBy(How = How.XPath, Using = "//*[@id='login_form']/button/span")]
         IWebElement signIn;
 
-        public IWebDriver Driver { get; private set; }
+        [FindsBy(How = How.Id, Using = "invoice_header_expand")]
+        IWebElement expand;
+
+        [FindsBy(How = How.ClassName, Using = "next_page")]
+        IWebElement nextPage;
+
+        [FindsBy(How = How.ClassName, Using = "primary")]
+        IList <IWebElement> pageMenu;
+      
+        [FindsBy(How = How.XPath, Using = "//*[@id='invoice_header_table_tag']")]
+        IList<IWebElement> invoiceHeaderandBody;
+        
+        [FindsBy(How = How.XPath, Using = "//*[@id='invoice_header_table_tag']/thead")]
+        IList <IWebElement> invoiceTableHeader;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='invoice_header_tbody']")]
+     //   [FindsBy(How = How.XPath, Using = "//*[@id='invoice_header_table_tag']/tbody")]
+        IList <IWebElement> invoiceTableBody;
+
+        //*[@id="invoice_header_tbody"]
+        [FindsBy(How = How.XPath, Using = "//*[@id='ch_invoice_number']")]
+        IWebElement invoiceButton;
+
+
         #endregion
 
         #region Methods
@@ -53,7 +76,83 @@ namespace Screens.Pages
         {
             w.Quit();
         }
+        public void expandTable()
+        {
+            expand.Click();
+        }
+        public void sortInvoices()
+        {
+            invoiceButton.Click();
+        }
+        public void getNextPage()
+        {
+            nextPage.Click();
+        }
+        public IList<IWebElement> pagesMenu()
+        {
+            return pageMenu;
+        }
+        public IList<IWebElement> invTableHeader()
+        {
+            return invoiceTableHeader;
+        }
+        public IList<IWebElement> invTableBody()
+        {
+            return invoiceTableBody;
+        }
+        public IList<IWebElement> invHeadandBody()
+        {
+            return invoiceHeaderandBody;
+        }
+
+        public List<IWebElement> body() {
+
+            List<IWebElement> bodyList = new List<IWebElement>();
+            // element contains all of the rows of the Table body
+            foreach (var element in invoiceTableBody)
+            {
+                // trlist contains an array[0]-[89] rows of data when foreach loop is entered
+                List<IWebElement> rowList = element.FindElements(By.TagName("tr")).ToList();
+                foreach (var row in rowList)
+                {
+                  //  Thread.Sleep(200);
+                    List<IWebElement> dataList = row.FindElements(By.TagName("td")).ToList();
+                    // cell is the element in the row
+                    foreach (var cell in dataList)
+                    {
+                        cell.GetAttribute("td");
+                      //  bodyList.Add(cell.Text);
+                    }
+                    
+                }
+            }
+            return bodyList;
+        }
+        public List<String> header() {
+
+            List<String> headerList = new List<String>();
+            
+            // foreach (var element in invoiceTableBody)
+            // element has - Invoice # Supplier Created By Created Date Payment Due Date Total With Taxes 
+            // Status Invoice Source Delivery Method Supplier Default Commodity Unanswered Comments Actions
+            foreach (var element in invoiceTableHeader)
+            {   // trList is a list of [0] has - Invoice # Supplier Created By Created Date Payment Due Date Total With Taxes 
+                // Status Invoice Source Delivery Method Supplier Default Commodity Unanswered Comments Actions
+                List<IWebElement> rowList = element.FindElements(By.TagName("tr")).ToList();
+                foreach (var row in rowList)
+                {                         
+                    List<IWebElement> headingList = row.FindElements(By.TagName("th")).ToList();
+                    foreach (var heading in headingList)
+                    {
+                         headerList.Add(heading.Text);
+                 //        heading.Click();
+                    }
+                }
+            }
+            return headerList;
+        }
         #endregion
+      
         #region Snapshot
         protected override void snapshot() { }
         #endregion
